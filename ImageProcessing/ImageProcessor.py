d23c63b15742967b8a057cad3f789d8968c83416
yonash24 <yonashay87@gmail.com>
2025-01-06 22:06:58 +0200

    creating the structor of the classes

--- ImageProcessing/ImageProcessor.py
+++ ImageProcessing/ImageProcessor.py
@@ -1,2 +1,28 @@
+import numpy as np
+import matplotlib as mt
+from PIL import Image
+
 class ImageProcessor:
-    pass
+    
+    def load_image():
+        pass
+    
+    def save_image():
+        pass
+    
+    def convert_to_grayscale():
+        pass
+    
+    def apply_filter():
+        pass
+    
+    def detect_edges():
+        pass
+    
+    def adjust_brightness():
+        pass
+    
+    
+    
+img = Image.open("Images/image1.jpeg")
+img.show()
--- ImageProcessing/main.py
+++ ImageProcessing/main.py
@@ -1,4 +1,7 @@
-class main:
-    pass
+import numpy as np
+import matplotlib as mt
+from PIL import Image
 
+img = Image.open("Images/image1.jpeg")
+img.show()
 
